6ea68db9de1b8a02ee114d4fb358950631051572
Kagura66b <61072668+Kagura66b@users.noreply.github.com>
2021-01-14 08:23:56 -0500

    updated to work in channels

--- src/main/java/Handlers/MessageHandler.java
+++ src/main/java/Handlers/MessageHandler.java
@@ -1,5 +1,6 @@
 package Handlers;
 
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +19,19 @@ public class MessageHandler extends ListenerAdapter {
                 event.getChannel().sendMessage("This command is used to check the odds of success given either 2 dice pools or a dice pool and a threshold. To test against a threshold use -1 as the limit in the second dataset. Current test parameters are 10000 reps").queue();
             }else{
                 event.getJDA().addEventListener(new DMStatsHandler(event));
+            }
+        }
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        String[] messageArgs = event.getMessage().getContentRaw().split(" ");
+        System.out.println("receivedMessage");
+        if(messageArgs[0].toLowerCase().equals("!statcheck")){
+            if(event.getMessage().getContentRaw().toLowerCase().equals("!statcheck help")){
+                event.getChannel().sendMessage("This command is used to check the odds of success given either 2 dice pools or a dice pool and a threshold. To test against a threshold use -1 as the limit in the second dataset. Current test parameters are 10000 reps").queue();
+            }else{
+                event.getJDA().addEventListener(new GuildStatsHandler(event));
             }
         }
     }
